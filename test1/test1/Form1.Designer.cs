3c80f2f1619e342086db094aad60601ce5d02293
MO <rickymodog@yahoo.com.hk>
2016-03-03 02:51:48 +0800

    update

--- test1/test1/Form1.Designer.cs
+++ test1/test1/Form1.Designer.cs
@@ -32,25 +32,31 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 200F));
             this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.listBox1, 2, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.82208F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.17791F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(454, 326);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(646, 324);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // pictureBox1
@@ -59,7 +65,7 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(448, 263);
+            this.pictureBox1.Size = new System.Drawing.Size(440, 245);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -68,9 +74,9 @@
             // 
             this.button1.AutoSize = true;
             this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Location = new System.Drawing.Point(3, 272);
+            this.button1.Location = new System.Drawing.Point(3, 254);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(221, 51);
+            this.button1.Size = new System.Drawing.Size(217, 46);
             this.button1.TabIndex = 1;
             this.button1.Text = "Play";
             this.button1.UseVisualStyleBackColor = true;
@@ -80,19 +86,38 @@
             // 
             this.button2.AutoSize = true;
             this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Location = new System.Drawing.Point(230, 272);
+            this.button2.Location = new System.Drawing.Point(226, 254);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(221, 51);
+            this.button2.Size = new System.Drawing.Size(217, 46);
             this.button2.TabIndex = 2;
             this.button2.Text = "Pause";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.textBox1, 2);
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox1.Location = new System.Drawing.Point(3, 306);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(440, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(449, 3);
+            this.listBox1.Name = "listBox1";
+            this.tableLayoutPanel1.SetRowSpan(this.listBox1, 3);
+            this.listBox1.Size = new System.Drawing.Size(194, 318);
+            this.listBox1.TabIndex = 4;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(454, 326);
+            this.ClientSize = new System.Drawing.Size(646, 324);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Media Player";
@@ -109,6 +134,8 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- test1/test1/Form1.cs
+++ test1/test1/Form1.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Media;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,66 +8,183 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 using AForge.Video.FFMPEG;
-using System.Timers;
-using NAudio;
+using Multimedia;
 using NAudio.Wave;
+using AviFile;
+using Accord.DirectSound;
 
 namespace test1
 {
     public partial class Form1 : Form
     {
-        private VideoFileReader reader = new VideoFileReader();
-        private System.Timers.Timer t;
-        private Bitmap[] buf;
-        private double fps;
-        private double duration;
-        private WaveStream wave;
-        private int buf_i;
+        private Multimedia.Timer t;
+        private AviManager manager;
+        int avi;
+
+        private bool hasAudio;
+        private AudioStream astream;
+        private Avi.AVISTREAMINFO astreamInfo;
+        private Avi.PCMWAVEFORMAT astreamFormat;
+        private IntPtr waveData;
+        private int astreamLength,astream_i;
+
+        private VideoFileReader reader;
+        private Bitmap[] vbuf;
+        private int h, w, vbuf_i; 
+        private long  frame_i,max_frame;
+        private int fps, duration;
+
+        private IWaveProvider wave;
+        private AudioOutputDevice audio_out;
+        private MemoryStream audio_stream;
+        private int sample_rate, sample_size,channels;
+        private Byte[][] abuf;
+        private int abuf_size,abuf_i;
 
         public Form1()
         {
             InitializeComponent();
-            buf_i = 0;
-            buf = new Bitmap[2];
+            Avi.AVIFileInit();
+            avi = 0;
+            hasAudio = false;
+            astreamLength = 0;
+            reader = new VideoFileReader();
+            vbuf = new Bitmap[2];
+            abuf = new Byte[2][];
+            t = new Multimedia.Timer();
+            t.Tick += new System.EventHandler(this.nextFrame);
+            t.SynchronizingObject = this;
+            t.Mode = TimerMode.Periodic;
         }
 
-        private void nextFrame(Object source, ElapsedEventArgs e)
+        private void nextFrame(Object source, System.EventArgs e)
         {
-            t.Stop();
-            if (buf[buf_i] != null)
+            if (vbuf[vbuf_i] != null)
             {
-                pictureBox1.Image = buf[buf_i];
-                t.Start();
-                buf[buf_i] = reader.ReadVideoFrame();
-                buf_i = 1 - buf_i;
-            }
-                
+
+                pictureBox1.Image = vbuf[vbuf_i];
+                if(frame_i<=max_frame)
+                {
+                    vbuf[vbuf_i] = reader.ReadVideoFrame();
+                    frame_i++;
+                }
+                else
+                {
+                    vbuf[vbuf_i] = null;
+                }
+                vbuf_i = 1 - vbuf_i;
+            }              
             else
             {
                 t.Dispose();
-                reader.Close();
+                manager.Close();
             }
+/*
+            if(abuf[abuf_i] != null)
+            {
+                textBox1.Text = sample_rate.ToString();
+                if (astream_i<(astreamLength-abuf_size))
+                {
+                    Marshal.Copy(waveData, abuf[abuf_i], astream_i, abuf_size);
+                    astream_i += abuf_size;
+                }
+                else if(astream_i<astreamLength)
+                {
+                    Marshal.Copy(waveData, abuf[abuf_i], astream_i, astreamLength-astream_i);
+                    astream_i += abuf_size;
+                }
+                else
+                {
+                    abuf[abuf_i] = null;
+                }
+                abuf_i = 1 - abuf_i;
+            }
+*/         
                 
+        }
+
+        private void play(String filepath)
+        {
+            if(avi==0)
+            {
+                Avi.AVIFILEINFO aviHeader = new Avi.AVIFILEINFO();
+                int streams;
+                int res = Avi.AVIFileOpen(ref avi, filepath, Avi.OF_READWRITE, 0);
+                if(res==0)
+                {
+                    if(avi != 0)
+                    {
+                        Avi.AVIFileInfo(avi, ref aviHeader, Marshal.SizeOf(aviHeader));
+                        streams = aviHeader.dwStreams;
+                        manager = new AviManager(filepath, true);
+                        if (streams == 2)
+                        {
+                            hasAudio = true;
+                        }
+                    }
+                }
+/*
+                if(hasAudio)
+                {
+                    astream = manager.GetWaveStream();
+                    sample_rate = astream.CountSamplesPerSecond;
+                    sample_size = astream.CountBitsPerSample;
+                    channels = astream.CountChannels;
+                    abuf_size = duration * sample_rate * sample_size / 8000;
+                    float[] temp = new float[astreamLength / 4];
+                    waveData = astream.GetStreamData(ref astreamInfo, ref astreamFormat, ref astreamLength);
+                    Marshal.Copy(waveData, temp, astream_i, astreamLength/4-1);
+                    audio_out = new AudioOutputDevice(Handle, sample_rate, channels);
+                    audio_out.Play(temp);
+
+                    abuf_i = 0;
+                    abuf[0] = new Byte[abuf_size];
+                    abuf[1] = new Byte[abuf_size];
+                    astreamInfo = new Avi.AVISTREAMINFO();
+                    astreamFormat = new Avi.PCMWAVEFORMAT();
+                    waveData = astream.GetStreamData(ref astreamInfo, ref astreamFormat, ref astreamLength);
+                    Byte[] temp = new byte[astreamLength];
+                    Marshal.Copy(waveData, temp, astream_i, astreamLength);
+                    using (MemoryStream ms = new MemoryStream(temp))
+                    {
+                         SoundPlayer player = new SoundPlayer(audio_stream);
+                         player.Play();
+                    }
+                    
+
+
+                }
+*/
+                if (!reader.IsOpen)
+                {
+                    reader.Open("H:\\SHE_uncompressed.avi");
+                    if (reader.IsOpen)
+                    {
+                        max_frame = reader.FrameCount;
+                        frame_i = 0;
+                        vbuf_i = 0;
+                        vbuf[0] = reader.ReadVideoFrame();
+                        vbuf[1] = reader.ReadVideoFrame();
+                        fps = reader.FrameRate;
+                        duration = 1000 / fps;
+                        t.Period = duration;
+
+                    }
+                }
+
+            }
+           t.Start();
+
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(!reader.IsOpen)
-            {
-                reader.Open("H:\\SHE_uncompressed.avi");
-                buf[0] = reader.ReadVideoFrame();
-                buf[1] = reader.ReadVideoFrame();
-            }
-            fps = (double)reader.FrameRate;
-            duration = 1 / fps * 1000;
-            t = new System.Timers.Timer(duration);
-            t.Elapsed += nextFrame;
-            t.AutoReset = false;
-            t.Enabled = true;
-
-
+            play("H:\\SHE_uncompressed.avi");
         }
 
         private void button2_Click(object sender, EventArgs e)
